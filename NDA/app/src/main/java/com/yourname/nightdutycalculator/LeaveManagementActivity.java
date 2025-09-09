fe91f617d42e91549c01423803e34c3f2112e59b
sainikhem1008-hue <sainikhem1008@gmail.com>
2025-09-09 12:35:37 +0530

    Update LeaveManagementActivity.java

--- NDA/app/src/main/java/com/yourname/nightdutycalculator/LeaveManagementActivity.java
+++ NDA/app/src/main/java/com/yourname/nightdutycalculator/LeaveManagementActivity.java
@@ -106,20 +106,3 @@ public class LeaveManagementActivity extends AppCompatActivity {
 }
 
 
-public class LeaveModel {
-    private int days;
-    private String type;
-
-    public LeaveModel(int days, String type) {
-        this.days = days;
-        this.type = type;
-    }
-
-    public int getDays() {
-        return days;
-    }
-
-    public String getType() {
-        return type;
-    }
-}
